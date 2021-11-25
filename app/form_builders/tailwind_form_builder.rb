a5e7fad13e66039d92d8bd364ca1193faf6a7b10
Luan Vieira <luan@hey.com>
2021-11-24 20:37:25 -0800

    Update users styling to match events

--- app/form_builders/tailwind_form_builder.rb
+++ app/form_builders/tailwind_form_builder.rb
@@ -11,6 +11,12 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
     super(object_name, merged_options)
   end
 
+  def email_field(object_name, options = {})
+    default_options = {class: "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm focus:outline-none"}
+    merged_options = default_options.merge(options)
+    super(object_name, merged_options)
+  end
+
   def text_area(object_name, options = {})
     default_options = {rows: 3, class: "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm focus:outline-none"}
     merged_options = default_options.merge(options)
@@ -28,7 +34,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
       with_css_classes: true,
       order: [:month, :day, :year, :hour, :minute],
       datetime_separator: "<div class='ml-8'></div>",
-      time_separator: "",
+      time_separator: ""
     }
     super(object_name, default_options.merge(options))
   end
